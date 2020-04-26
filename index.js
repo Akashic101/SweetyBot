82f1ffd0999cfe439c9d870160f5462868fe2774
David Moll <david.leander.moll@gmail.com>
2020-04-26 13:46:54 +0200

    Pushed first version

---
+++ index.js
@@ -0,0 +1,58 @@
+const tmi = require('tmi.js');
+
+const options = {
+    options: {
+        debug: true,
+    },
+    connection: {
+        cluster: 'aws',
+        reconnect: true,
+    },
+    identity: {
+        username: 'SweetyBot',
+        password: 'oauth:20yw252ciaqck24hj613w2dodlrlot',
+    },
+    channels: ['Redfur_13'],
+};
+
+const client = new tmi.client(options);
+
+client.connect();
+
+client.on('connected', (address, port) => {
+    client.action('Redfur_13', 'SweetyBot is now online');
+});
+
+client.on('chat', (channel, user, message, self) => {
+    if(self) return;
+    const commandmessage = message.trim().toLowerCase();
+
+    if (commandmessage === '!instagram' || commandmessage === '!insta') {
+        client.say('Redfur_13', 'https://www.instagram.com/sweetycomics')
+    }
+
+    if (commandmessage === '!discord') {
+        client.say('Redfur_13', 'https://discordapp.com/invite/U4rymnU')
+    }
+
+    if (commandmessage === '!facebook' || commandmessage === '!fb') {
+        client.say('Redfur_13', 'https://www.facebook.com/Redfur13-2323949451264229/?ref=aymt_homepage_panel&eid=ARBQBOOYhk572IzVWcoV08jK4-y8bf8sSRWQ-KRxqrryne0yGezIFTfZzgmEUg78Xn3D0VU15YKeTW2A')
+    }
+
+    if (commandmessage === '!youtube' || commandmessage === '!yt') {
+        client.say('Redfur_13', 'https://www.youtube.com/channel/UCEefUPort7KP98FpTQGv91A')
+    }
+
+    if (commandmessage === '!twitter') {
+        client.say('Redfur_13', 'https://twitter.com/redfur13')
+    }
+
+    if (commandmessage === '!webtoon' || commandmessage === '!webtoons') {
+        client.say('Redfur_13', 'https://www.webtoons.com/en/challenge/sweety-comics/list?title_no=389966')
+    }
+
+    if(commandmessage === 'fiverr') {
+        client.say('Redfur_13', 'https://www.fiverr.com/share/9dNkgd')
+    }
+
+});
